a84fc5a2b984cc61bfffb5f5ec4ab48fb47cbd37
John DeVitis <johndevitis@gmail.com>
2016-07-22 11:01:57 -0400

    error screened path handling

--- @classy/classy.m
+++ @classy/classy.m
@@ -35,6 +35,15 @@ classdef classy < matlab.mixin.SetGet
         % fcn 
         end
         
+        function strip_folder(self)
+        % remove @class_name from object path if present
+        % note - updates instance of self.path only
+            ind = regexp(self.path,'@','once');  % find location of @ folder
+            if ~isempty(ind) % if present, remove
+                self.path = self.path(1:ind-1);                
+            end
+        end
+        
         function write_propd(self,cname)
         % generate boiler plate syntax for dependent properties of classdef
         % files. optional input of internal class reference name (default
